3a90947cb8f2605b6a944e656bb9aae12dc2de9d
my <user@debian.my.com>
2019-02-01 21:13:40 +0800

    init

---
+++ src/Helper.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 19-2-1
+ * Time: 下午9:12
+ */
+
+namespace Xiaoxiaoxiaox\Xixixi;
+
+/**
+ * 助手函数类
+ * Class Helper
+ * @package Xiaoxiaoxiaox\Xixixi
+ */
+class Helper
+{
+    public function index()
+    {
+        return 'helper';
+    }
+}
